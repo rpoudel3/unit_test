b88d586bb76764eb0595bf95771784d0e6f1e5ff
Rashmi Poudel <rashmipoudel@rashmis-mbp.attlocal.net>
2016-07-24 21:22:13 -0700

    starting a test case file

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+from api.views import get_data, pca_var
+import numpy as np
+from unit_test import requester.py
+from fun_things import add
+from numpy.testing import assert_array_almost_equal
+
+
+class TestNumComponentsReturned(unittest.TestCase):
+
+    def test_add(self):
+        res = add(3, 4)
+        self.assertEqual(res, 7)
+
+    def test_add_fails_when_input_is_string(self):
+        with self.assertRaises(TypeError):
+            add(3, 'hello')
+
+    def test_numpy_array_almost_equal(self):
+        arr1 = np.array([0.0, 0.10000000, 0.15])
+        arr2 = np.array([0.0, 0.10000089, 0.15])
+        assert_array_almost_equal(arr1, arr2, decimal=6)
+
+class TestSomeOtherThing(unittest.TestCase):
+
+    def test_something_else(self):
+        pass

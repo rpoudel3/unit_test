301f238197f4f065fff65b36904798d59ecb7823
Rashmi Poudel <rashmipoudel@Rashmis-MacBook-Pro.local>
2016-07-31 15:55:42 -0700

    wanted to check the filepath

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class TestURL(unittest.TestCase):
         url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
         returned_fname = requester.url_to_csv(url, fname="test_fname.csv")
         self.assertIsInstance(returned_fname, str)
-        self.assertEqual(returned_fname, "/Users/rashmipoudel/Desktop/Git/unit_test/test_fname.csv")
+        self.assertEqual(returned_fname, "test_fname.csv")
 
     def test_invalid_url(self):
         # test to see if the url passed in is invalid.
@@ -26,24 +26,24 @@ class TestURL(unittest.TestCase):
         readerobject=requester.url_to_csv(url, fname='fname.csv')
         self.assertTrue(str(type(readerobject)),"_csv.reader")
 
-    def test_invalid_csv(self):
-        #test to see if the valid url has an invalid csv.
-        url="http://stackoverflow.com/questions/17730173/python-cant-get-full-path-name-of-file"
-        with self.assertRaises(TypeError):
-            requester.url_to_csv(url,'/Users/rashmipoudel/Desktop/Git/unit_test/tester.csv')
+    # def test_invalid_csv(self):
+    #     #test to see if the valid url has an invalid csv.
+    #     url="http://stackoverflow.com/questions/17730173/python-cant-get-full-path-name-of-file"
+    #     with self.assertRaises(TypeError):
+    #         requester.url_to_csv(url,'/Users/rashmipoudel/Desktop/Git/unit_test/tester.csv')
 
 
 class Test_batch_URL_csv(unittest.TestCase):
-    def test_valid_url(self):
-         #emits a runtime warning indicating that the invalid CSV URL or URL that cannot be accessed was skipped.
-
-        url = ["http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
-                "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer-data",
-               "http://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv"]
-        returned_fname = requester.batch_url_to_csv(url, fnames=["test", "test2","test3"])
-        self.assertEqual(returned_fname, ["/Users/rashmipoudel/Desktop/Git/unit_test/test",
-                                          "/Users/rashmipoudel/Desktop/Git/unit_test/test2",
-                                          "/Users/rashmipoudel/Desktop/Git/unit_test/test3"])
+    # def test_valid_url(self):
+    #      #emits a runtime warning indicating that the invalid CSV URL or URL that cannot be accessed was skipped.
+    #
+    #     url = ["http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
+    #             "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer-data",
+    #            "http://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv"]
+    #     returned_fname = requester.batch_url_to_csv(url, fnames=["test", "test2","test3"])
+    #     self.assertEqual(returned_fname, ["/Users/rashmipoudel/Desktop/Git/unit_test/test",
+    #                                       "/Users/rashmipoudel/Desktop/Git/unit_test/test2",
+    #                                       "/Users/rashmipoudel/Desktop/Git/unit_test/test3"])
 
     def test_number_of_files(self):
         #unittest no.4
@@ -57,15 +57,15 @@ class Test_batch_URL_csv(unittest.TestCase):
          number_files=len(returned_fname)
          self.assertEqual(number_files, 1)
 
-    def test_correct_filenames(self):
-        #unittest no.6
-        url = ["https://docs.travis-ci.com/user/languages/python",
-                "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer-data",
-               "http://google.com/student"]
-        returned_fname=requester.batch_url_to_csv(url,fnames=['t1',
-                                                              't2',
-                                                              't3'])
-        self.assertEqual(returned_fname,['/Users/rashmipoudel/Desktop/Git/unit_test/t2'])
+    # def test_correct_filenames(self):
+    #     #unittest no.6
+    #     url = ["https://docs.travis-ci.com/user/languages/python",
+    #             "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer-data",
+    #            "http://google.com/student"]
+    #     returned_fname=requester.batch_url_to_csv(url,fnames=['t1',
+    #                                                           't2',
+    #                                                           't3'])
+    #     self.assertEqual(returned_fname,['/Users/rashmipoudel/Desktop/Git/unit_test/t2'])
 
     def test_content_file(self):
         url=["http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data",
@@ -78,17 +78,17 @@ class Test_batch_URL_csv(unittest.TestCase):
 
 
 
-    def test_correct_number_of_filenames(self):
-        #unittest no.7
-        url = ["https://docs.travis-ci.com/user/languages/python",
-                "https://github.com/pydata/pandas/issues/10153",
-               "http://stackoverflow.com/questions/17730173/python-cant-get-full-path-name-of-file"]
-
-        returned_fname=requester.batch_url_to_csv(url, fnames=["test_fname.csv",
-                                                               "test2_fname.csv",
-                                                               "test3_fname.csv"])
-        number_files=len(returned_fname)
-        self.assertEqual(number_files, 0)
+    # def test_correct_number_of_filenames(self):
+    #     #unittest no.7
+    #     url = ["https://docs.travis-ci.com/user/languages/python",
+    #             "https://github.com/pydata/pandas/issues/10153",
+    #            "http://stackoverflow.com/questions/17730173/python-cant-get-full-path-name-of-file"]
+    #
+    #     returned_fname=requester.batch_url_to_csv(url, fnames=["test_fname.csv",
+    #                                                            "test2_fname.csv",
+    #                                                            "test3_fname.csv"])
+    #     number_files=len(returned_fname)
+    #     self.assertEqual(number_files, 0)
 
     def test_duplicate_URLS(self):
         #unittest no.8

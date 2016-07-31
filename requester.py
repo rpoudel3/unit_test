27074840d88d9b1be8f80f93c7752a460302b781
Rashmi Poudel <rashmipoudel@rashmis-mbp.attlocal.net>
2016-07-30 20:32:33 -0700

    more changes to the functions

--- requester.py
+++ requester.py
@@ -12,43 +12,45 @@ def url_to_csv(url, fname):
     and saves it to a file"""
 
     try:
-        u = urllib2.urlopen(url)
-        f = open(fname, 'w')
-        f.write(u.read())
+        data = pd.read_csv(url)
+        data.to_csv(fname)
+        #response=urllib2.urlopen(url)
+        #cr=csv.reader(response)
     except urllib2.URLError:
         raise ValueError('Invalid URL')
-
-    with open(fname,'rb')as g:
-        try:
-                dialect=csv.Sniffer().sniff(g.read(1024))
-                g.seek(0)
-        except csv.Error:
-            #sys.exit('file %s, line %d: %s' %(fname, reader.line_num, e))
-            raise TypeError('Invalid CSV')
-    g.close()
+    except IOError:
+        raise TypeError
+    except pd.parser.CParserError:
+        raise TypeError('URL Cannot be Parsed as CSV')
     return fname
 
-url_to_csv('http://stackoverflow.com/questions/17730173/python-cant-get-full-path-name-of-file','fname')
-    
+#url_to_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/breast-cancer-data','fname')
+
 def batch_url_to_csv(urls, fnames):
     """Takes a list of URLs to CSV files, downloads them, and 
     saves them to files given by the list of names in fnames.
     Returns a list of the filenames saved."""
     list_of_files=[]
+    unique=set(urls)
+    if len(unique)!=len(urls):
+        raise AssertionError("Duplicate URLS cannot be present in the parameter 'urls'")
     for i in range(len(urls)):
-        try:
-            list_of_files.append(url_to_csv(urls[i],fnames[i]))
-        except ValueError:
-                warnings.warn('RuntimeWarning,%s has been skipped' % (urls[i]))
+            try:
+                list_of_files.append(url_to_csv(urls[i],fnames[i]))
+            except TypeError:
+                    warnings.warn('RuntimeWarning,%s has been skipped' % (urls[i]))
+            except ValueError:
+                    warnings.warn('RuntimeWarning,%s has been skipped' % (urls[i]))
                 #skip=True
                 #continue
     return list_of_files
+
          
 def url_to_df(url):
     """Takes a URL to a CSV file and returns the contents of the URL 
     as a Pandas DataFrame.
     """
-  
+
     data_frame=pd.read_csv(url)
     return data_frame
     
